882fbf51e009f2e4a39ed03d69fb6eb171808275
Julius Neuffer <julius@i44pc45.info.uni-karlsruhe.de>
2010-08-24 15:25:06 +0200

    bugfix

--- frontend.c
+++ frontend.c
@@ -571,7 +571,7 @@ static ir_node *handle_call_expr(call_expr_t *call, parameter_t *args)
 		in = calloc(call->argc, sizeof(ir_node **));
 
 	for (prototype_t *p = prototypes; p != NULL; p = p->next) {
-		if (!strcmp(p->name, call->callee) && p->argc == call->argc) {
+		if (!strcmp(p->name, call->callee)) {
 			ent = p->ent;
 			callee = new_SymConst(get_modeP(), (symconst_symbol) ent, symconst_addr_ent);
 			break;
