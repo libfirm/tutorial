a63d356aa6a285e3add894c241855de45ccac16f
Matthias Braun <matze@braunis.de>
2011-11-10 16:43:00 +0100

    improve test runner

--- runtests.sh
+++ runtests.sh
@@ -2,11 +2,18 @@
 
 for src in `ls *.simple`
 do
-	echo "Compiling and running $src..."
-	`./tutorial $src`
+	echo -n "Compiling $src...  "
 	assembly=${src%.simple}.s
-	`gcc libsimple.o $assembly -o ${src%.simple}`
-	rm -f "${assembler}"
+	if ! `./tutorial $src`; then
+		echo "Compile error"
+	else
+		`gcc libsimple.o $assembly -o ${src%.simple}`
+		echo "ok"
+		echo "Running $src..."
+		./${src%.simple} < /dev/null
+		rm -f "${src%.simple}"
+	fi
+	rm -f "${assembly}"
 done
 
 exit 0
