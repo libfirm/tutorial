b37b066f363458ccbf916a0bd4d6f418e32902da
Matthias Braun <matze@braunis.de>
2011-12-13 13:32:34 +0100

    exit 1 if examples fail

--- runtests.sh
+++ runtests.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+RC=0
 for src in `ls *.simple`
 do
 	echo -n "Compiling $src...  "
 	assembly=${src%.simple}.s
 	if ! `./tutorial $src`; then
 		echo "Compile error"
+		RC=1
 	else
 		`gcc libsimple.o $assembly -o ${src%.simple}`
 		echo "ok"
 		echo "Running $src..."
-		./${src%.simple} < /dev/null
+		./${src%.simple} < /dev/null || RC=1
 		rm -f "${src%.simple}"
 	fi
 	rm -f "${assembly}"
 done
 
-exit 0
+exit $RC
